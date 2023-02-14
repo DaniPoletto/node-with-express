c0e43a5f916d0cda480605e074c8be153cbc8771
Daniela Poletto <danielapoletto_bep@hotmail.com>
2023-02-14 20:57:45 -0300

    creating routes to delete and update people

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -31,6 +31,34 @@ class PessoaController {
             return res.status(500).json(error.message)
         }
     }
+
+    static async update (req, res) {
+        const person = req.body
+        const { id } = req.params
+        try {
+            await database.People.update(person, { where : { 
+                id : Number(id)
+            }});
+            const personUpdated = await database.People.findOne( { where : { 
+                id : Number(id)
+            }})
+            return res.status(200).json(personUpdated)
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
+    static async delete (req, res) {
+        const { id } = req.params
+        try {
+            const personDeleted = await database.People.destroy( { where : { 
+                id : Number(id)
+            }});
+            return res.status(200).json(personDeleted)
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
 }
 
 module.exports = PessoaController
--- api/routes/peopleRoute.js
+++ api/routes/peopleRoute.js
@@ -6,5 +6,7 @@ const router = Router()
 router.get('/people', PessoaController.all)
 router.get('/people/:id', PessoaController.findOneById)
 router.post('/people', PessoaController.create)
+router.put('/people/:id', PessoaController.update)
+router.delete('/people/:id', PessoaController.delete)
 
 module.exports = router
