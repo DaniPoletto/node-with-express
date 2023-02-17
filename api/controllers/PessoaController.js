326b7630809702f1e1d8eb95c83f0f97886b6afe
Daniela Poletto <danielapoletto_bep@hotmail.com>
2023-02-16 21:10:06 -0300

    creating crud to enrollment

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -1,4 +1,4 @@
-const database = require('../models')
+const database = require('../models/index')
 
 class PessoaController {
     static async all(req, res) {
@@ -51,10 +51,64 @@ class PessoaController {
     static async delete (req, res) {
         const { id } = req.params
         try {
-            const personDeleted = await database.People.destroy( { where : { 
+            await database.People.destroy( { where : { 
                 id : Number(id)
             }});
             return res.status(200).json({ message: `id ${id} was deleted`})
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
+    static async findOneEnrollment (req, res) {
+        const { studentId, enrollmentId } = req.params
+        try {
+            const enrollment = await database.Enrollments.findOne( { where : { 
+                id : Number(enrollmentId),
+                student_id : Number(studentId)
+            }})
+            return res.status(200).json(enrollment)
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
+    static async createEnrollment (req, res) {
+        const { studentId } = req.params
+        const newEnrollment = { ...req.body, student_id: Number(studentId) }
+        try {
+            const newEnrollmentCreated = await database.Enrollments.create(newEnrollment);
+            return res.status(200).json(newEnrollmentCreated)
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
+    static async updateEnrollment (req, res) {
+        const { studentId, enrollmentId } = req.params
+        const enrollment = req.body
+        try {
+            await database.Enrollments.update(enrollment, { where : { 
+                id : Number(enrollmentId),
+                student_id : Number(studentId)
+            }});
+            const enrollmentUpdated = await database.Enrollments.findOne( { where : { 
+                id : Number(enrollmentId)
+            }})
+            return res.status(200).json(enrollmentUpdated)
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
+    static async deleteEnrollment (req, res) {
+        const { studentId, enrollmentId } = req.params
+        try {
+            await database.Enrollments.destroy( { where : { 
+                id : Number(enrollmentId),
+                student_id : Number(studentId)
+            }});
+            return res.status(200).json({ message: `id ${enrollmentId} was deleted`})
         } catch (error) {
             return res.status(500).json(error.message)
         }
--- api/routes/peopleRoute.js
+++ api/routes/peopleRoute.js
@@ -8,5 +8,9 @@ router.get('/people/:id', PessoaController.findOneById)
 router.post('/people', PessoaController.create)
 router.put('/people/:id', PessoaController.update)
 router.delete('/people/:id', PessoaController.delete)
+router.get('/people/:studentId/enrollments/:enrollmentId', PessoaController.findOneEnrollment)
+router.post('/people/:studentId/enrollments', PessoaController.createEnrollment)
+router.put('/people/:studentId/enrollments/:enrollmentId', PessoaController.updateEnrollment)
+router.delete('/people/:studentId/enrollments/:enrollmentId', PessoaController.deleteEnrollment)
 
 module.exports = router
